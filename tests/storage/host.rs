3c60a51c1fc38012355c4948f2ce8d1098e5c1d9
Toby Davis <pencilcaseman@gmail.com>
2024-06-12 22:33:10 +0100

    Fix test suite

--- tests/storage/host.rs
+++ tests/storage/host.rs
@@ -1,6 +1,6 @@
 use rayon::prelude::*;
 use std::hint::black_box;
-use tensr::backend::host::storage;
+use tensr::backend::host::host_storage;
 
 macro_rules! test_all {
     ($macro_name:ident, $($type:ty),+) => {
@@ -23,11 +23,14 @@ macro_rules! test_alloc {
         #[test]
         fn $name() {
             let n = 1000;
-            let s = storage::HostStorage::<$type>::new(n);
+            let s = host_storage::HostStorage::<$type>::new(n);
             assert_eq!(s.length, n);
 
             // Assert alignment is correct
-            assert_eq!((s.ptr.0.as_ptr() as usize) % storage::MEM_ALIGN, 0);
+            assert_eq!(
+                (s.ptr.0.as_ptr() as usize) % host_storage::MEM_ALIGN,
+                0
+            );
 
             for i in 0..s.length {
                 type Type = $type;
@@ -42,70 +45,21 @@ macro_rules! test_alloc_uninit {
         #[test]
         fn $name() {
             let n = 1000;
-            let mut s = unsafe { storage::HostStorage::<$type>::new_uninit(n) };
+            let mut s =
+                unsafe { host_storage::HostStorage::<$type>::new_uninit(n) };
             assert_eq!(s.length, n);
 
             // Assert alignment is correct
-            assert_eq!((s.ptr.0.as_ptr() as usize) % storage::MEM_ALIGN, 0);
+            assert_eq!(
+                (s.ptr.0.as_ptr() as usize) % host_storage::MEM_ALIGN,
+                0
+            );
 
             // Check we can write to this data without segfaulting
             for i in 0..s.length {
                 type Type = $type;
                 s[i] = Type::default();
                 assert_eq!(s[i], Type::default());
-            }
-        }
-    };
-}
-
-macro_rules! test_as_shared {
-    ($type:ty, $name:ident) => {
-        #[test]
-        fn $name() {
-            let n = 1000;
-            let s = storage::HostStorage::<$type>::new(n);
-            let shared = s.as_shared();
-
-            assert_eq!(shared.length, n);
-
-            // Assert alignment is correct (correct, since it references the same pointer)
-            assert_eq!(
-                (shared.ptr.0.as_ptr() as usize) % storage::MEM_ALIGN,
-                0
-            );
-
-            // Check all values are valid and correct
-            for i in 0..shared.length {
-                type Type = $type;
-                assert_eq!(shared[i], { Type::default() });
-            }
-        }
-
-        paste::paste! {
-            #[test]
-            fn [<$name _mut>]() {
-                let n = 1000;
-                let mut s = storage::HostStorage::<$type>::new(n);
-                let mut shared = s.as_shared();
-
-                assert_eq!(shared.length, n);
-
-                // Assert alignment is correct (correct, since it references the same pointer)
-                assert_eq!(
-                    (shared.ptr.0.as_ptr() as usize) % storage::MEM_ALIGN,
-                    0
-                );
-
-                // Set values in shared and then verify in s
-                for i in 0..shared.length {
-                    type Type = $type;
-                    shared[i] = Type::MAX;
-                }
-
-                for i in 0..shared.length {
-                    type Type = $type;
-                    assert_eq!(s[i], Type::MAX);
-                }
             }
         }
     };
@@ -120,7 +74,7 @@ macro_rules! test_take_as_vec {
 
             // Drop s to check the memory is not freed
             {
-                let mut s = storage::HostStorage::<$type>::new(n);
+                let mut s = host_storage::HostStorage::<$type>::new(n);
                 v = s.take_as_vec();
                 drop(s);
             }
@@ -141,14 +95,14 @@ macro_rules! test_simd_par_iter {
         #[test]
         fn $name() {
             let n_simd = 1000;
-            let n = n_simd * storage::SIMD_WIDTH;
-            let s = storage::HostStorage::<$type>::new(n);
+            let n = n_simd * host_storage::SIMD_WIDTH;
+            let s = host_storage::HostStorage::<$type>::new(n);
 
             type Type = $type;
 
             (0..n).into_par_iter().zip(s.simd_par_iter()).for_each(
                 |(idx, packet)| {
-                    for i in 0..storage::SIMD_WIDTH {
+                    for i in 0..host_storage::SIMD_WIDTH {
                         assert_eq!(packet[i], Type::default());
                     }
                 },
@@ -162,8 +116,8 @@ macro_rules! test_slice_par_iter {
         #[test]
         fn $name() {
             let n_simd = 1000;
-            let n = n_simd * storage::SIMD_WIDTH;
-            let s = storage::HostStorage::<$type>::new(n);
+            let n = n_simd * host_storage::SIMD_WIDTH;
+            let s = host_storage::HostStorage::<$type>::new(n);
 
             type Type = $type;
 
@@ -171,7 +125,7 @@ macro_rules! test_slice_par_iter {
                 .into_par_iter()
                 .zip(s.slice_par_iter(storage::SIMD_WIDTH))
                 .for_each(|(idx, slice)| {
-                    for i in 0..storage::SIMD_WIDTH {
+                    for i in 0..host_storage::SIMD_WIDTH {
                         assert_eq!(slice[i], Type::default());
                     }
                 });
@@ -184,16 +138,16 @@ macro_rules! test_slice_mut_par_iter {
         #[test]
         fn $name() {
             let n_simd = 1000;
-            let n = n_simd * storage::SIMD_WIDTH;
-            let mut s = unsafe { storage::HostStorage::<$type>::new_uninit(n); }
+            let n = n_simd * host_storage::SIMD_WIDTH;
+            let mut s = unsafe { host_storage::HostStorage::<$type>::new_uninit(n); }
 
             type Type = $type;
 
             (0..n)
                 .into_par_iter()
-                .zip(s.slice_mut_par_iter(storage::SIMD_WIDTH))
+                .zip(s.slice_mut_par_iter(host_storage::SIMD_WIDTH))
                 .for_each(|(idx, slice)| {
-                    for i in 0..storage::SIMD_WIDTH {
+                    for i in 0..host_storage::SIMD_WIDTH {
                         slice[i] = Type::default();
                         assert_eq!(slice[i], Type::default());
                     }
@@ -210,7 +164,7 @@ macro_rules! test_drop {
 
             // Create a LOT of these and see if the system runs out of memory...
             for _ in 0..10_000 {
-                let s = black_box(storage::HostStorage::<$type>::new(n));
+                let s = black_box(host_storage::HostStorage::<$type>::new(n));
                 drop(s);
             }
         }
@@ -219,7 +173,6 @@ macro_rules! test_drop {
 
 test_all_fundamental!(test_alloc);
 test_all_fundamental!(test_alloc_uninit);
-test_all_fundamental!(test_as_shared);
 test_all_fundamental!(test_take_as_vec);
 test_all_fundamental!(test_simd_par_iter);
 test_all_fundamental!(test_drop);
